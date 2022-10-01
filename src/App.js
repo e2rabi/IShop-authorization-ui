331afbf5e0b7fcb0bcc008e8ebe20aa496500282
errabi ayoub <errabi.ayoub@gmail.com>
2022-10-01 22:02:34 +0100

    add parcel config

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import { ReactDOM } from "react-dom";
+
 const User = (props) => {
     return React.createElement("div", {}, [
         React.createElement("h2", {}, props.name),
