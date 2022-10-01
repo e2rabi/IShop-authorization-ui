0ff9162701f25ced1bff1eb9b088bf291fadae6d
errabi ayoub <errabi.ayoub@gmail.com>
2022-10-01 17:36:39 +0100

    setup prettier formater

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
+const User = (props) => {
+    return React.createElement("div", {}, [
+        React.createElement("h2", {}, props.name),
+        React.createElement("h2", {}, "Khalil"),
+        React.createElement("h2", {}, "Nassim"),
+        React.createElement("h2", {}, "Ayoub"),
+    ]);
+};
+const App = () => {
+    return React.createElement("div", {}, [
+        React.createElement("h2", { id: "title" }, "Login"),
+        React.createElement(User, { name: "ERRABI AMINE" }),
+        React.createElement(User),
+    ]);
+};
 
-const User = (props)=>{
-    return (
-        React.createElement("div",{},[
-            React.createElement("h2",{},props.name),
-            React.createElement("h2",{},"Khalil"),
-            React.createElement("h2",{},"Nassim"),
-            React.createElement("h2",{},"Ayoub"),
-        ])
-    );
-}
-const App = () =>{
-    return React.createElement("div",
-    {}
-    ,[React.createElement("h2",{id:"title"},"Login"),
-      React.createElement(User,{name:"ERRABI AMINE"}),
-      React.createElement(User)
-    ]
-    );
-  };
-
-  ReactDOM.render(React.createElement(App),document.getElementById("root"));
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
