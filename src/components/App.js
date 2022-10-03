7e069afa2dc7a7e48b14136edd3fe197f50012d0
errabi ayoub <errabi.ayoub@gmail.com>
2022-10-03 22:22:23 +0100

    add eslint config to support jsx

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,18 @@
-import React from "react";
 import User from "./User"
 
-const App = () => {
+/* const App = () => {
     return React.createElement("div", {}, [
         React.createElement("h2", { id: "title" }, "Login"),
         React.createElement(User, { name: "ERRABI AMINE" }),
         React.createElement(User),
     ]);
+}; */
+const App = ()=>{
+    return (
+        <div>
+            <User name="Errabi Amine"/>
+        </div>
+    );
 };
 
 export default App;
--- src/components/User.js
+++ src/components/User.js
@@ -1,9 +1,19 @@
-import React from "react";
+//import React from "react";
 
-const User = (props) => {
+/* const User = (props) => {
   return   React.createElement("div",{},[
            React.createElement("h1",{},props.name),
     ]);
+};
+ */
+// using jsx
+
+const User = (props) =>{
+  return(
+    <div>
+      <h1>{props.name}</h1>
+    </div>
+  );
 };
 
 export default User;
